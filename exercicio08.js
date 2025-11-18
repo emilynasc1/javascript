d3e6660f56d785f4b8bfc6c782d9e613364be657
EMILY DO NASCIMENTO LIMA <14252835941@imaccommand016.local>
2025-11-18 16:13:09 -0300

    Exercicio08

---
+++ exercicio08.js
@@ -0,0 +1,25 @@
+let peso = parseFloat(prompt("Informe seu peso (em kg): "))
+let altura = parseFloat(prompt("Informe a sua altura (em metros: "))
+
+let imc = peso / (altura*altura)
+
+console.log("Seu IMC é:" + imc)
+
+if(imc < 18.5){
+    console.log("Abaixo do peso")
+}
+else if(imc >= 18.5 && imc <= 24.9){
+    console.log("Peso normal")
+}
+else if(imc >= 25 && imc <= 29.9){
+    console.log("Sobrepeso")
+}
+else if(imc >= 30 && imc <= 34.9){
+    console.log("Obesidade grau 1")
+}
+else if(imc >= 35 && imc <=39.9){
+    console.log("Obesidade grau 2")
+}
+else{
+    console.log("Obesidade grau 3")
+}
